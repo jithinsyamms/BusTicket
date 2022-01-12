9581988d7fd6ce5324dbb909ba85d4f86a728003
jithin <jithinsyamms@gmail.com>
2022-01-12 12:24:47 +0530

    Initial Commit

---
+++ BusTicket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusTicket
+//
+//  Created by Jithin on 12/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
